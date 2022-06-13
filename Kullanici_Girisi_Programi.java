71d2e1aefaa10d2489ab00ce53d04f8d234be22c
Ilhan Darici <ilhan.dar33gmail.com>
2022-06-13 03:05:38 +0300

    try-finally added

--- Kullanici_Girisi_Programi.java
+++ Kullanici_Girisi_Programi.java
@@ -5,43 +5,46 @@ public class Kullanici_Girisi_Programi {
         Scanner input = new Scanner (System.in);
         String username, password, inpUsername = "Patika", yesNo,
          inpPassword = "Patika123";
-        
-        System.out.print("Kullanıcı adını giriniz : ");
-        username = input.nextLine();
-        System.out.print("Şifrenizi giriniz : ");
-        password = input.nextLine();
+        try{
+            System.out.print("Kullanıcı adını giriniz : ");
+            username = input.nextLine();
+            System.out.print("Şifrenizi giriniz : ");
+            password = input.nextLine();
 
-        if(username.equals(inpUsername) && password.equals(inpPassword)){
-            System.out.println("Başarılı bir şekilde giriş yaptınız !");
-        }
-        else{
-            System.out.print("Kullanıcı adı veya şifre yanlış !\nŞifrenizi değiştirmek istiyor musunuz ?\nEvet için (Y) hayır için (N) basınız. (Y/N) : ");
-            yesNo = input.nextLine();
-
-            switch (yesNo) {
-                case "Y":
-                case "y":
-                    System.out.print("Yeni şifrenizi giriniz : ");
-                    password = input.nextLine();
-                    
-                    if(password.equals(inpPassword)){
-                        System.out.println("Yeni şifreniz eski şifrenizle aynı olamaz !");
-                    }
-                    else{
-                        System.out.println("Şifreniz başarılı bir şekilde oluşturuldu !");
-                    }
-                    break;
-                case "N":
-                case "n":
-                    System.out.println("Ana menüye dönülüyor...");
-                    break;
-                default:
-                    System.out.println("Hatalı bir tuşlama yaptınız !");
-                    break;
+            if(username.equals(inpUsername) && password.equals(inpPassword))    {
+                System.out.println("Başarılı bir şekilde giriş yaptınız     !");
             }
+            else{
+                System.out.print("Kullanıcı adı veya şifre yanlış !\nŞifrenizi değiştirmek istiyor musunuz ?\nEvet için (Y) hayır için (N) basınız. (Y/N) : ");
+                yesNo = input.nextLine();
+
+                switch (yesNo) {
+                    case "Y":
+                    case "y":
+                        System.out.print("Yeni şifrenizi giriniz : ");
+                        password = input.nextLine();
+                    
+                        if(password.equals(inpPassword)){
+                        System.out.println("Yeni şifreniz eski şifrenizle aynı olamaz !");
+                        }
+                        else{
+                        System.out.println("Şifreniz başarılı bir şekilde oluşturuldu !");
+                        }
+                        break;
+                    case "N":
+                    case "n":
+                        System.out.println("Ana menüye dönülüyor...");
+                        break;
+                    default:
+                        System.out.println("Hatalı bir tuşlama yaptınız     !");
+                        break;
+                }
+            }
+        }finally{
+            input.close();
         }
-        
-        
     }
-    
+        
 }
+    
+
